e24e76c631fe4865500500adfafbcd33b4110b5d
Colin Eberhardt <colin.eberhardt@gmail.com>
2016-01-21 18:14:59 +0000

    Improved configuration

--- index.js
+++ index.js
@@ -10,11 +10,10 @@ const yaml = require('js-yaml');
 const curry = require('curry');
 
 const util = require('./util');
+
+//TODO: destructuring!
 const merge = util.merge, chainPromises = util.chainPromises, writeFile = util.writeFile,
     readFile = util.readFile, mapFilePaths = util.mapFilePaths, mapFiles = util.mapFiles;
-// const {
-//     merge, chainPromises, writeFile, readFile, mapFilePaths, mapFiles
-// } = require('util');
 
 const handlebarsCompile = memoize(handlebars.compile);
 
@@ -24,8 +23,8 @@ marked.setOptions({
     }
 });
 
-function loadHandlebarsPartials() {
-    return mapFilePaths('_includes/*.hbs', file => {
+function loadHandlebarsPartials(includesFolder) {
+    return mapFilePaths(`${includesFolder}/*.hbs`, file => {
         const templateName = path.basename(file, '.hbs');
         const template = fs.readFileSync(file, 'utf8');
         handlebars.registerPartial(templateName, template);
@@ -33,8 +32,15 @@ function loadHandlebarsPartials() {
     });
 }
 
-function renderLayout(postMatter, layoutName) {
-    layoutName = layoutName || postMatter.data.layout;
+
+const renderLayout = curry(
+    function renderLayout(postMatter) {
+        return renderNamedLayout(postMatter, postMatter.data.layout);
+    }
+);
+
+
+function renderNamedLayout(postMatter, layoutName) {
     const layoutFile = `_layouts/${layoutName}.hbs`;
     return readFile(layoutFile)
         .then(file => matter(file))
@@ -45,9 +51,8 @@ function renderLayout(postMatter, layoutName) {
             const mergedData = merge(layoutMatter.data, postMatter.data, {body: postMatter.rendered});
             const rendered = layoutTemplate(mergedData);
             const newMatter = merge(postMatter, { rendered });
-
             if (layoutMatter.data.layout) {
-                return renderLayout(newMatter, layoutMatter.data.layout);
+                return renderNamedLayout(newMatter, layoutMatter.data.layout);
             } else {
                 return newMatter;
             }
@@ -55,17 +60,19 @@ function renderLayout(postMatter, layoutName) {
 }
 
 // create a page variable that contains filepath information'
-const addPageMetadata = curry(function addPageMetadata(filePath, postMatter) {
-    // '/foo/bar/baz/asdf/quux.md'
-    const page = {
-        path: filePath, // '/foo/bar/baz/asdf/quux.md'
-        basename: path.basename(filePath, path.extname(filePath)), // 'quux'
-        dirname: path.dirname(filePath),  // '/foo/bar/baz/asdf'
-        ext: path.extname(filePath), // '.md'
-        destination: path.join('/', filePath.substring(0, filePath.length - path.extname(filePath).length) + '.html') // '/foo/bar/baz/asdf/quux.html'
-    };
-    return merge(postMatter, { data: {page} });
-});
+const addPageMetadata = curry(
+    function addPageMetadata(filePath, postMatter) {
+        // '/foo/bar/baz/asdf/quux.md'
+        const page = {
+            path: filePath, // '/foo/bar/baz/asdf/quux.md'
+            basename: path.basename(filePath, path.extname(filePath)), // 'quux'
+            dirname: path.dirname(filePath),  // '/foo/bar/baz/asdf'
+            ext: path.extname(filePath), // '.md'
+            destination: path.join('/', filePath.substring(0, filePath.length - path.extname(filePath).length) + '.html') // '/foo/bar/baz/asdf/quux.html'
+        };
+        return merge(postMatter, { data: {page} });
+    }
+);
 
 // renders the template in the 'content' property with the 'data' into a 'rendered' property
 function renderTemplate(postMatter) {
@@ -80,52 +87,69 @@ function renderMarkdown(postMatter) {
     return merge(postMatter, { rendered });
 }
 
-const writePost = curry(function writePost(destinationPath, postMatter) {
-    const dest = path.join(destinationPath, postMatter.data.page.destination);
-    console.log('writing file', dest);
-    return writeFile(dest, postMatter.rendered);
-});
+const writePost = curry(
+    function writePost(destinationFolder, postMatter) {
+        const dest = path.join(destinationFolder, postMatter.data.page.destination);
+        console.log('writing file', dest);
+        return writeFile(dest, postMatter.rendered);
+    }
+);
 
-const addGlobalData = curry(function addGlobalData(globalData, postMatter) {
-    return merge(postMatter, { data: globalData});
-});
+const mergeGlobalData = curry(
+    function mergeGlobalData(globalData, postMatter) {
+        return merge(postMatter, { data: globalData});
+    }
+);
 
-const collectPagesFrontMatter = curry(function collectPagesFrontMatter(filePattern, globalData) {
-    return mapFilePaths(filePattern, filePath => {
-        return readFile(filePath)
-            .then(file => matter(file))
-            .then(postMatter => addPageMetadata(filePath, postMatter))
-            .then(postMatter => {
-                return {
-                    page: postMatter.page,
-                    data: postMatter.data
-                };
-            });
-    }).then(pages => merge(globalData, { pages }));
-});
+const collectPagesFrontMatter = curry(
+    function collectPagesFrontMatter(filePattern, globalData) {
+        return mapFilePaths(filePattern, filePath => {
+            return readFile(filePath)
+                .then(file => matter(file))
+                .then(postMatter => addPageMetadata(filePath, postMatter))
+                .then(postMatter => {
+                    return {
+                        page: postMatter.page,
+                        data: postMatter.data
+                    };
+                });
+        }).then(pages => merge(globalData, { pages }));
+    }
+);
 
-function loadGlobalData(filePattern, globalData) {
-    return mapFiles(filePattern, (file, filePath) => {
-        return {
-            [path.basename(filePath, path.extname(filePath))]: yaml.safeLoad(file)
-        };
-    }).then(globals => merge(globalData, ...globals));
-}
+const loadGlobalData = curry(
+    function loadGlobalData(filePattern, globalData) {
+        return mapFiles(filePattern, (file, filePath) => {
+            return {
+                [path.basename(filePath, path.extname(filePath))]: yaml.safeLoad(file)
+            };
+        }).then(globals => merge(globalData, ...globals));
+    }
+);
 
-function build(filePattern, destinationPath, globalPattern) {
-    return chainPromises(loadHandlebarsPartials(), [
-        loadGlobalData(globalPattern),
-        collectPagesFrontMatter(filePattern)
+const defaultConfig = {
+    includesFolder: '_includes',
+    globalPattern: [],
+    filePattern: ['**/*.md'],
+    destinationFolder: '_site'
+};
+
+function build(config) {
+    config = merge(defaultConfig, config || {});
+
+    return chainPromises(loadHandlebarsPartials(config.includesFolder), [
+        loadGlobalData(config.globalPattern),
+        collectPagesFrontMatter(config.filePattern)
     ])
     .then((globalData) => {
-        return mapFiles(filePattern, (file, filePath) => {
+        return mapFiles(config.filePattern, (file, filePath) => {
             return chainPromises(matter(file), [
-                addGlobalData(globalData),
+                mergeGlobalData(globalData),
                 addPageMetadata(filePath),
                 renderTemplate,
                 renderMarkdown,
-                renderLayout,
-                writePost(destinationPath)
+                renderLayout(),
+                writePost(config.destinationFolder)
             ]);
         });
     });
